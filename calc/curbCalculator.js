d79f96c50e03020db4a8a7644890003a115f61b8
paulbowser <wxsniper@gmail.com>
2023-12-05 16:57:25 -0600

    Update curbCalculator.js

--- calc/curbCalculator.js
+++ calc/curbCalculator.js
@@ -1,1 +1,20 @@
- 
+document.getElementById('curbForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  var thickness = parseFloat(document.getElementById('thicknessInput').value);
+  var height = parseFloat(document.getElementById('heightInput').value);
+  var length = parseFloat(document.getElementById('lengthInput').value);
+  var costPerCubicYard = parseFloat(document.getElementById('costPerCubicYardInput').value);
+
+  var crbcuft = ((thickness / 12.0) * (height / 12.0) * length);
+  var crbcuyd = crbcuft / 27.0;
+  
+  var totalCost = crbcuyd * costPerCubicYard;
+
+  var results = "<h2>Results</h2>" +
+    "Cubic Feet: " + crbcuft.toFixed(2) + "<br>" +
+    "Cubic Yards: " + crbcuyd.toFixed(2) + "<br>" +
+    "Total Cost: $" + totalCost.toFixed(2);
+
+  document.getElementById('results').innerHTML = results;
+});
