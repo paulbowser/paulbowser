8b459de759f7e582eb1a454455e619cc9452f179
paulbowser <wxsniper@gmail.com>
2023-12-05 14:49:00 -0600

    Update adaCalculator.js
    Fixed cost calc

--- calc/adaCalculator.js
+++ calc/adaCalculator.js
@@ -3,24 +3,23 @@ document.getElementById('circleForm').addEventListener('submit', function(event)
 
   var rad = parseFloat(document.getElementById('radiusInput').value);
   var mcir = parseFloat(document.getElementById('circumferenceInput').value);
-  var cost = parseFloat(document.getElementById('costInput').value);
+  var costPerSqFt = parseFloat(document.getElementById('costPerSqFtInput').value);
 
   var pi = Math.PI;
-  var area = (pi * rad * rad).toFixed(2);
-  var circu = (2 * pi * rad).toFixed(2);
-  var percircu = (mcir / circu).toFixed(2);
-  var actsqft = (area * percircu).toFixed(2);
-  var cuyd = ((actsqft * 0.5) / 27).toFixed(2);
-  var costPerSqFt = (cost / actsqft).toFixed(2);
+  var area = pi * rad * rad;
+  var circu = 2 * pi * rad;
+  var percircu = mcir / circu;
+  var actsqft = area * percircu;
+  var totalCost = actsqft * costPerSqFt;
 
   var results = "<h2>Results</h2>" +
     "Radius: " + rad + "<br>" +
     "Measured Circle: " + mcir + "<br>" +
-    "Area of Circle: " + area + "<br>" +
-    "Circumference: " + circu + "<br>" +
-    "Percent of Circumference: " + percircu + "<br>" +
-    "Actual Sq.Ft.: " + actsqft + "<br>" +
-    "Cubic Yards: " + cuyd + "<br>" +
-    "Cost Per Square Foot: $" + costPerSqFt;
+    "Area of Circle: " + area.toFixed(2) + "<br>" +
+    "Circumference: " + circu.toFixed(2) + "<br>" +
+    "Percent of Circumference: " + percircu.toFixed(2) + "<br>" +
+    "Actual Sq.Ft.: " + actsqft.toFixed(2) + "<br>" +
+    "Total Cost: $" + totalCost.toFixed(2);
+
   document.getElementById('results').innerHTML = results;
 });
