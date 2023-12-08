6f9b2344447dbc6a988804e81c023f6a65ec42fe
paulbowser <wxsniper@gmail.com>
2023-12-08 13:19:04 -0600

    Update curbCalculator.js

--- calc/curbCalculator.js
+++ calc/curbCalculator.js
@@ -10,7 +10,8 @@ document.getElementById('curbForm').addEventListener('submit', function(event) {
   var cuft = (height / 12) * length * (thickness / 12);
 
   var results = "<h2>Results</h2>" +
-    "Cubic Feet: " + cuft.toFixed(2) + " <br>" +
+    "Total Material Volume in Cubic Feet: " + cuft.toFixed(2) + "<br>" +
+    "Total Linear Feet: " + length.toFixed(2) + "<br>" + // Display entered length as total linear feet
     "Total Cost: $" + totalCost.toFixed(2);
 
   document.getElementById('results').innerHTML = results;
