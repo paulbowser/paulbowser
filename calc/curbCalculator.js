9f1a85dcffc2ffa20cc533b8650431ec80ae0fb9
paulbowser <wxsniper@gmail.com>
2023-12-08 13:08:05 -0600

    Update curbCalculator.js
    Changed to linear feet cost calc

--- calc/curbCalculator.js
+++ calc/curbCalculator.js
@@ -1,19 +1,16 @@
 document.getElementById('curbForm').addEventListener('submit', function(event) {
   event.preventDefault();
 
-  var thickness = parseFloat(document.getElementById('thicknessInput').value);
   var height = parseFloat(document.getElementById('heightInput').value);
   var length = parseFloat(document.getElementById('lengthInput').value);
-  var costPerCubicYard = parseFloat(document.getElementById('costPerCubicYardInput').value);
+  var thickness = parseFloat(document.getElementById('thicknessInput').value);
+  var costPerLinearFt = parseFloat(document.getElementById('costPerLinearFtInput').value);
 
-  var crbcuft = ((thickness / 12.0) * (height / 12.0) * length);
-  var crbcuyd = crbcuft / 27.0;
-  
-  var totalCost = crbcuyd * costPerCubicYard;
+  var totalCost = length * costPerLinearFt;
+  var cuft = (height / 12) * length * (thickness / 12);
 
   var results = "<h2>Results</h2>" +
-    "Cubic Feet: " + crbcuft.toFixed(2) + "<br>" +
-    "Cubic Yards: " + crbcuyd.toFixed(2) + "<br>" +
+    "Cubic Feet: " + cuft.toFixed(2) + " <br>" +
     "Total Cost: $" + totalCost.toFixed(2);
 
   document.getElementById('results').innerHTML = results;
